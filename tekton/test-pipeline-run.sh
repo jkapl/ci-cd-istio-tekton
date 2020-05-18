4b6c711510eaca84936fa1cc605c626c1044a765
Joel Kaplan <joelkaplan1@gmail.com>
2020-05-18 14:11:22 -0700

    Add tekton resources for pipelines, pvc and tasks

---
+++ tekton/test-pipeline-run.sh
@@ -0,0 +1,1 @@
+tkn pipeline start build-deploy-canary --param="GIT_URL=https://github.com/jkapl/ci-cd-istio-tekton" --param="BUILDER_IMAGE=quay.io/buildah/stable:v1.14.8" --param="REVISION=master" --showlog
